67f284198d70337a5117fa12c120cadbfd6a33f2
PrvSunKS <48259084+PrvSunKS@users.noreply.github.com>
2020-04-22 12:42:41 +0530

    First Commit
    First Commit

---
+++ src/main/java/com/toystore/ecomm/zuulsvr/utils/UserContextInterceptor.java
@@ -0,0 +1,24 @@
+package com.toystore.ecomm.zuulsvr.utils;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+public class UserContextInterceptor implements ClientHttpRequestInterceptor {
+    @Override
+    public ClientHttpResponse intercept(
+            HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
+            throws IOException {
+
+        HttpHeaders headers = request.getHeaders();
+        headers.add(UserContext.CORRELATION_ID, UserContextHolder.getContext().getCorrelationId());
+        headers.add(UserContext.AUTH_TOKEN, UserContextHolder.getContext().getAuthToken());
+        System.out.println("Response Byte Array: " + body.toString());
+        
+        return execution.execute(request, body);
+    }
+}
